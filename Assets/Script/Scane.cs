699b13e495ac4bb6f78818fb4db0c88702605cf0
elyas <elyasdwimaryanto27@gmail.com>
2019-12-21 15:10:25 +0700

    commit1

---
+++ Assets/Script/Scane.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scane : MonoBehaviour
+{
+    public void CallSceneAR(){
+		Application.LoadLevel("scene_ar");
+	}
+}
